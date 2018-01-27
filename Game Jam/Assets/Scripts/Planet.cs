2fb70b91e1109badb0e7db25450f6e58d90c0d39
resident5 <Valencio_Williams@Bloomfield.edu>
2018-01-26 19:52:05 -0500

    Folders

---
+++ Game Jam/Assets/Scripts/Planet.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+	#region Enemy variables
+	public Planet enemy;
+	#endregion
+
+	#region Player Variables
+	public float health;
+	#endregion
+
+	public GameObject virus;
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Z))
+		{
+			Shoot ();
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		
+
+	}
+
+	void Shoot ()
+	{
+		if (virus != null)
+		{
+			GameObject shot = Instantiate (virus, Vector3.zero, Quaternion.identity);
+			if (shot.GetComponent<Virus> ())
+			{
+				shot.GetComponent<Virus> ().player = this;
+			}
+		}
+	}
+
+	public void TakeDamage(int dmg)
+	{
+		health -= dmg;
+	}
+}
---
+++ Game Jam/Assets/Scripts/Virus.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virus : MonoBehaviour
+{
+
+	public Planet player;
+
+	public Planet enemy;
+
+	public Rigidbody rb;
+
+	public float rotateSpeed;
+
+	public float speed;
+
+	public int dmg;
+
+	void Start ()
+	{
+		enemy = player.enemy;
+	}
+
+	void LateUpdate ()
+	{
+		Vector3 pos = Vector3.MoveTowards (player.transform.position,player.enemy.transform.position, 100f);
+
+		transform.position += Vector3.Lerp (player.transform.position, pos, 0.005f);
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject != player)
+		{
+			if (other.gameObject == enemy)
+			{
+				enemy.TakeDamage (dmg);
+			}
+		}
+	}
+
+	void CalculateRotation()
+	{
+		Vector3 dir = player.enemy.gameObject.transform.position - player.gameObject.transform.position;
+		dir.Normalize ();
+
+		float rotateAmtX = Vector3.Cross (dir, transform.right).x;
+		float rotateAmtY = Vector3.Cross (dir, transform.up).y;
+		float rotateAmtZ = Vector3.Cross (dir, transform.forward).z;
+
+		Vector3 rotationAmt = new Vector3 (rotateAmtX, rotateAmtY, rotateAmtZ);
+
+		rb.angularVelocity = rotationAmt * rotateSpeed;
+
+		rb.velocity = transform.forward * speed;
+
+	}
+}
